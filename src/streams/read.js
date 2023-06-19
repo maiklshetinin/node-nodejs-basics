e03ca62de8d2a2210d87b94233c1f038f960a2f6
maiklshetinin <sh_chetinin@mail.ru>
2023-06-20 00:00:39 +0300

    feat: add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { ReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const read = ReadStream(join(__dirname, 'files', 'fileToRead.txt'));
+    read.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
