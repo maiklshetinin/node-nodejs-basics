d46afd25fcd2957b7ecce5e19413682243ac0e9e
maiklshetinin <sh_chetinin@mail.ru>
2023-06-19 23:57:08 +0300

    feat: add cp and zip

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,11 +1,11 @@
 import { dirname, join } from 'path';
-import { spawn } from 'child_process';
 import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const spawnChildProcess = async (args) => {
-    
+
     const cp = spawn('node', [join(__dirname, 'files', 'script.js'), ...args]);
 
     process.stdin.pipe(cp.stdin);
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createUnzip } from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+    const input = createReadStream(join(__dirname, 'files', 'archive.gz'));
+    const output = createWriteStream(
+        join(__dirname, 'files', 'fileToCompress.txt')
+    );
+    const decompress = createUnzip();
+    input.pipe(decompress).pipe(output);
 };
 
 await decompress();
