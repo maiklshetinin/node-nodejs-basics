f6a70e7ed7bd99b1b32564b00d06b991dcb607cd
maiklshetinin <sh_chetinin@mail.ru>
2023-06-19 23:49:15 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { dirname, join } from 'path';
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    
+    const cp = spawn('node', [join(__dirname, 'files', 'script.js'), ...args]);
+
+    process.stdin.pipe(cp.stdin);
+
+    cp.stdout.pipe(process.stdout);
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv.slice(2));
